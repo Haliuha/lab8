444ef6db3f8a3f6a4248d9060d9aee29f7559fc5
Haliuha <73331262+Haliuha@users.noreply.github.com>
2020-10-23 14:55:26 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log('sf');
+
+// function Mendchilgee(props) { console.log(props)
+//     return (
+//         <div>
+//             <h1>{props.ner} </h1>
+//             <p> {props.nas} </p>
+//             <p> {props.surchbgaEseh ? "unen" : "choloo awsan"} </p>
+//             <p> {props.dunguud[0]} </p>
+//             <p> {props.dunguud[1]}</p>
+//             <p> {props.dunguud[2]}</p>
+
+//             { props.dunguud.map(
+//                 (el,index) =>
+//                     <p key= {index}>{el}</p>
+//                 )
+//             }
+
+//             <p> {props.myObj.mergejil} </p>
+//             <p> {props.myObj.index}</p>
+//         </div>
+//     );
+// }
+// ReactDOM.render(
+//     // <h1>hjohu</h1>,
+//     <Mendchilgee ner={"Hello Haliun"}
+//     nas={20}
+//     suechbgaEseh={true}
+//     dunguud={[80,"dalan yos", true]}
+//     myObj={ {mergejil: 'SW', index:'SW18D040'} }/>,
+//     document.getElementById('root')
+// );
